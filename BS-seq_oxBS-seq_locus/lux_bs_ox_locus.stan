0385533de9dbde4c70e932461a98dfce0ed31140
tare <tarmo.aijo@gmail.com>
2016-03-15 11:40:17 -0400

    Added files via upload

---
+++ BS-seq_oxBS-seq_locus/lux_bs_ox_locus.stan
@@ -0,0 +1,140 @@
+data {
+  real mu_mu_bsEff;
+  real sigma_mu_bsEff;
+  
+  real mu_sigma_bsEff;
+  real sigma_sigma_bsEff;
+
+  real mu_mu_bsBEff;
+  real sigma_mu_bsBEff;
+
+  real mu_sigma_bsBEff;
+  real sigma_sigma_bsBEff;
+
+  real mu_mu_oxEff;
+  real sigma_mu_oxEff;
+
+  real mu_sigma_oxEff;
+  real sigma_sigma_oxEff;
+
+  real mu_mu_seqErr;
+  real sigma_mu_seqErr;
+
+  real mu_sigma_seqErr;
+  real sigma_sigma_seqErr;
+
+  real g_a;
+  real g_b;
+
+  int<lower=1> R;
+  int<lower=1> N;
+  int<lower=1> N_control;
+
+  int<lower=0> bsC[N,R];
+  int<lower=0> bsTot[N,R];
+  int<lower=0> oxC[N,R];
+  int<lower=0> oxTot[N,R];
+
+  int<lower=0> bsC_control[N_control,R];
+  int<lower=0> bsTot_control[N_control,R];
+  int<lower=0> oxC_control[N_control,R];
+  int<lower=0> oxTot_control[N_control,R];
+
+  vector<lower=0>[3] alpha;
+
+  vector<lower=0>[3] alpha_control[N_control];
+}
+parameters {
+  real mu_bsEff;
+  real<lower=0> sigma_bsEff;
+  real raw_bsEff[R];
+
+  real mu_bsBEff;
+  real<lower=0> sigma_bsBEff;
+  real raw_bsBEff[R];
+
+  real mu_oxEff;
+  real<lower=0> sigma_oxEff;
+  real raw_oxEff[R];
+
+  real mu_seqErr;
+  real<lower=0> sigma_seqErr;
+  real raw_seqErr[R];
+
+  real<lower=0> g;
+  real<lower=0> f;
+
+  simplex[3] mu;
+  simplex[3] nu[N];
+  simplex[3] theta[N,R];
+
+  simplex[3] theta_control[N_control,R];
+}
+transformed parameters {
+  real<lower=0,upper=1> bsEff[R];
+  real<lower=0,upper=1> bsBEff[R];
+  real<lower=0,upper=1> oxEff[R];
+  real<lower=0,upper=1> seqErr[R];
+
+  vector<lower=0>[3] mug;
+
+  mug <- g*mu+1;
+
+  for (r in 1:R) {
+    bsEff[r]  <- inv_logit(mu_bsEff  + sigma_bsEff  * raw_bsEff[r]);
+    bsBEff[r] <- inv_logit(mu_bsBEff + sigma_bsBEff * raw_bsBEff[r]);
+    oxEff[r]  <- inv_logit(mu_oxEff  + sigma_oxEff  * raw_oxEff[r]);
+    seqErr[r] <- inv_logit(mu_seqErr + sigma_seqErr * raw_seqErr[r]);
+  }
+}
+model {
+  for (n in 1:N) {
+    nu[n] ~ dirichlet(mug);
+    for (r in 1:R) {
+      theta[n,r] ~ dirichlet(f*nu[n]+1);
+      bsC[n,r] ~ binomial(bsTot[n,r],
+              theta[n,r,1]*((1.0 - seqErr[r])*(1.0 - bsEff[r]) + seqErr[r] * bsEff[r]) +
+              theta[n,r,2]*((1.0 - bsBEff[r])*(1.0 - seqErr[r]) + seqErr[r] * bsBEff[r]) +
+              theta[n,r,3]*((1.0 - bsBEff[r])*(1.0 - seqErr[r]) + seqErr[r] * bsBEff[r]));
+      oxC[n,r] ~ binomial(oxTot[n,r],
+              theta[n,r,1]*((1.0 - seqErr[r])*(1.0 - bsEff[r]) + seqErr[r] * bsEff[r]) +
+              theta[n,r,2]*((1.0 - bsBEff[r])*(1.0 - seqErr[r]) + seqErr[r] * bsBEff[r]) +
+              theta[n,r,3]*(oxEff[r]*((1.0 - seqErr[r])*(1.0 - bsEff[r]) + seqErr[r] * bsEff[r])+(1.0 - oxEff[r])*((1.0 - bsBEff[r])*(1.0 - seqErr[r]) + bsBEff[r] * seqErr[r])));
+    }
+  }
+
+  for (r in 1:R) {
+    for (n in 1:N_control) {
+      theta_control[n,r] ~ dirichlet(alpha_control[n]);
+      bsC_control[n,r] ~ binomial(bsTot_control[n,r],
+              theta_control[n,r,1]*((1.0 - seqErr[r])*(1.0 - bsEff[r]) + seqErr[r] * bsEff[r]) +
+              theta_control[n,r,2]*((1.0 - bsBEff[r])*(1.0 - seqErr[r]) + seqErr[r] * bsBEff[r]) +
+              theta_control[n,r,3]*((1.0 - bsBEff[r])*(1.0 - seqErr[r]) + seqErr[r] * bsBEff[r]));
+      oxC_control[n,r] ~ binomial(oxTot_control[n,r],
+              theta_control[n,r,1]*((1.0 - seqErr[r])*(1.0 - bsEff[r]) + seqErr[r] * bsEff[r]) +
+              theta_control[n,r,2]*((1.0 - bsBEff[r])*(1.0 - seqErr[r]) + seqErr[r] * bsBEff[r]) +
+              theta_control[n,r,3]*(oxEff[r]*((1.0 - seqErr[r])*(1.0 - bsEff[r]) + seqErr[r] * bsEff[r])+(1.0 - oxEff[r])*((1.0 - bsBEff[r])*(1.0 - seqErr[r]) + bsBEff[r] * seqErr[r])));
+    }
+  }
+
+  mu ~ dirichlet(alpha);
+
+  g ~ gamma(g_a,g_b);
+  f ~ gamma(g_a,g_b);
+
+  mu_bsEff ~ normal(mu_mu_bsEff, sigma_mu_bsEff);
+  sigma_bsEff ~ lognormal(mu_sigma_bsEff, sigma_sigma_bsEff);
+  raw_bsEff ~ normal(0,1);
+
+  mu_bsBEff ~ normal(mu_mu_bsBEff, sigma_mu_bsBEff);
+  sigma_bsBEff ~ lognormal(mu_sigma_bsBEff, sigma_sigma_bsBEff);
+  raw_bsBEff ~ normal(0,1);
+
+  mu_oxEff ~ normal(mu_mu_oxEff, sigma_mu_oxEff);
+  sigma_oxEff ~ lognormal(mu_sigma_oxEff, sigma_sigma_oxEff);
+  raw_oxEff ~ normal(0,1);
+
+  mu_seqErr ~ normal(mu_mu_seqErr, sigma_mu_seqErr);
+  sigma_seqErr ~ lognormal(mu_sigma_seqErr, sigma_sigma_seqErr);
+  raw_seqErr ~ normal(0,1);
+}
---
+++ BS-seq_oxBS-seq_locus/parse.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import numpy
+import scipy.stats
+import argparse
+
+def generate_output_files(data_file,prior_file,control_data_file,control_prior_file,prefix):
+
+  # hyperprior for bsEff
+  mu_mu_bsEff, sigma_mu_bsEff = 2, 1.29
+  mu_sigma_bsEff, sigma_sigma_bsEff = 0.4, 0.5
+  # hyperprior for bsBeff
+  mu_mu_bsBEff, sigma_mu_bsBEff = -3, 1.29
+  mu_sigma_bsBEff, sigma_sigma_bsBEff = 0.4, 0.5
+  # hyperprior for oxEff
+  mu_mu_oxEff, sigma_mu_oxEff = 2, 1.29
+  mu_sigma_oxEff, sigma_sigma_oxEff = 0.4, 0.5
+  # hyperprior for seqErr
+  mu_mu_seqErr, sigma_mu_seqErr = -3, 1.29
+  mu_sigma_seqErr, sigma_sigma_seqErr = 0.4, 0.5
+
+  # prior for g
+  g_a, g_b = 2.0, 2/6.0
+
+  # read the input files
+  data = numpy.loadtxt(data_file,delimiter='\t',skiprows=0,dtype='int')
+  prior = numpy.loadtxt(prior_file,delimiter='\t',skiprows=0,dtype='float')
+  control_data = numpy.loadtxt(control_data_file,delimiter='\t',skiprows=0,dtype='int')
+  control_prior = numpy.loadtxt(control_prior_file,delimiter='\t',skiprows=0,dtype='float')
+
+  # make sure the arrays are 2-dimensional
+  if len(data.shape) == 1:
+    data = numpy.reshape(data,[1,len(data)])
+  if len(prior.shape) == 1:
+    prior = numpy.reshape(prior,[1,len(prior)])
+  if len(control_data.shape) == 1:
+    control_data = numpy.reshape(control_data,[1,len(control_data)])
+  if len(control_prior.shape) == 1:
+    control_prior = numpy.reshape(control_prior,[1,len(control_prior)])
+
+  # check that the files were in the right format
+  if data.shape[1] % 4 != 0:
+    sys.exit('error: the number of columns in %s is not divisible by four'%(data_file))
+  if prior.shape[1] != 3:
+    sys.exit('error: there should be exactly three columns in %s'%(prior_file))
+  if prior.shape[0] != 1 :
+    sys.exit('error: there should be exactly one line in %s'%(prior_file))
+  if control_data.shape[1] % 4 != 0:
+    sys.exit('error: the number of columns in the file %s is not divisible by four'%(control_data_file))
+  if control_prior.shape[1] != 3:
+    sys.exit('error: there should be exactly three columns in %s'%(control_prior_file))
+
+  if control_data.shape[0] != control_prior.shape[0]:
+    sys.exit('error: the number of lines do not match in %s and %s'%(control_data_file,control_prior_file))
+    
+  # get the number of replicates
+  R = control_data.shape[1]/4
+  # get the number of control cytosines
+  N_control = control_data.shape[0]
+  # get the number of noncontrol cytosines
+  N = data.shape[0]
+
+  # get the number of C and total read-outs for noncontrol cytosines in BS-seq and oxBS-seq
+  bsC, bsTot, oxC, oxTot = data[:,0::4], data[:,1::4], data[:,2::4], data[:,3::4]
+
+  # get the number of C and total read-outs for control cytosines in BS-seq and oxBS-seq
+  bsC_control, bsTot_control, oxC_control, oxTot_control = control_data[:,0::4], control_data[:,1::4], control_data[:,2::4], control_data[:,3::4]
+
+  # print DATA
+  with open(prefix+'_data.R','w') as f:
+    f.write("mu_mu_bsEff <- %f\nsigma_mu_bsEff <- %f\nmu_sigma_bsEff <- %f\nsigma_sigma_bsEff <- %f\nmu_mu_bsBEff <- %f\nsigma_mu_bsBEff <- %f\nmu_sigma_bsBEff <- %f\nsigma_sigma_bsBEff <- %f\nmu_mu_oxEff <- %f\nsigma_mu_oxEff <- %f\nmu_sigma_oxEff <- %f\nsigma_sigma_oxEff <- %f\nmu_mu_seqErr <- %f\nsigma_mu_seqErr <- %f\nmu_sigma_seqErr <- %f\nsigma_sigma_seqErr <- %f\ng_a <- %f\ng_b <- %f\n" % (mu_mu_bsEff,sigma_mu_bsEff,mu_sigma_bsEff,sigma_sigma_bsEff,mu_mu_bsBEff,sigma_mu_bsBEff,mu_sigma_bsBEff,sigma_sigma_bsBEff,mu_mu_oxEff,sigma_mu_oxEff,mu_sigma_oxEff,sigma_sigma_oxEff,mu_mu_seqErr,sigma_mu_seqErr,mu_sigma_seqErr,sigma_sigma_seqErr,g_a,g_b))
+    f.write("N <- %d\nR <- %d\n" % (N,R))
+    f.write("N_control <- %d\n" % (N_control))
+    f.write("bsC <- structure(c(%s), .Dim=c(%d,%d))\n" % (','.join(map(str,bsC.flatten(1))),N,R))
+    f.write("bsTot <- structure(c(%s), .Dim=c(%d,%d))\n" % (','.join(map(str,bsTot.flatten(1))),N,R))
+    f.write("oxC <- structure(c(%s), .Dim=c(%d,%d))\n" % (','.join(map(str,oxC.flatten(1))),N,R))
+    f.write("oxTot <- structure(c(%s), .Dim=c(%d,%d))\n" % (','.join(map(str,oxTot.flatten(1))),N,R))
+
+    f.write("bsC_control <- structure(c(%s), .Dim=c(%d,%d))\n" % (','.join(map(str,bsC_control.flatten(1))),N_control,R))
+    f.write("bsTot_control <- structure(c(%s), .Dim=c(%d,%d))\n" % (','.join(map(str,bsTot_control.flatten(1))),N_control,R))
+    f.write("oxC_control <- structure(c(%s), .Dim=c(%d,%d))\n" % (','.join(map(str,oxC_control.flatten(1))),N_control,R))
+    f.write("oxTot_control <- structure(c(%s), .Dim=c(%d,%d))\n" % (','.join(map(str,oxTot_control.flatten(1))),N_control,R))
+
+    f.write("alpha <- c(%s)\n" % (','.join(map(str,prior.flatten(1)))))
+    f.write("alpha_control <- structure(c(%s), .Dim=c(%d,%d))\n" % (','.join(map(str,control_prior.flatten(1))),N_control,3))
+
+  # sample initial values from priors
+  mu_bsEff = scipy.stats.norm.rvs(mu_mu_bsEff, sigma_mu_bsEff)
+  sigma_bsEff = scipy.stats.lognorm.rvs(sigma_sigma_bsEff,loc=0,scale=numpy.exp(mu_sigma_bsEff))
+  mu_bsBEff = scipy.stats.norm.rvs(mu_mu_bsBEff, sigma_mu_bsBEff)
+  sigma_bsBEff = scipy.stats.lognorm.rvs(sigma_sigma_bsBEff,loc=0,scale=numpy.exp(mu_sigma_bsBEff))
+  mu_oxEff = scipy.stats.norm.rvs(mu_mu_oxEff, sigma_mu_oxEff)
+  sigma_oxEff = scipy.stats.lognorm.rvs(sigma_sigma_oxEff,loc=0,scale=numpy.exp(mu_sigma_oxEff))
+  mu_seqErr = scipy.stats.norm.rvs(mu_mu_seqErr, sigma_mu_seqErr)
+  sigma_seqErr = scipy.stats.lognorm.rvs(sigma_sigma_seqErr,loc=0,scale=numpy.exp(mu_sigma_seqErr))
+  raw_bsEff,raw_bsBEff,raw_oxEff,raw_seqErr = [],[],[],[]
+  for _ in range(0,R):
+    raw_bsEff.append(scipy.stats.norm.rvs(0,1))
+    raw_bsBEff.append(scipy.stats.norm.rvs(0,1))
+    raw_oxEff.append(scipy.stats.norm.rvs(0,1))
+    raw_seqErr.append(scipy.stats.norm.rvs(0,1))
+  g = numpy.random.gamma(g_a,1.0/g_b)
+  f_v = numpy.random.gamma(g_a,1.0/g_b)
+  theta = ','.join(numpy.array([map(str,numpy.random.dirichlet(row)) for row in numpy.tile(numpy.tile(prior,(N,1)),(R,1))]).flatten(1))
+  theta_control = ','.join(numpy.array([map(str,numpy.random.dirichlet(row)) for row in numpy.tile(control_prior,(R,1))]).flatten(1))
+  mu = ','.join(map(str,numpy.random.dirichlet(prior[0])))
+  nu = ','.join(numpy.array([map(str,numpy.random.dirichlet(prior[0])) for row in range(0,N)]).flatten(1))
+
+  # print INIT
+  with open(prefix+'_init.R','w') as f:
+    f.write("mu_bsEff <- %f\nsigma_bsEff <- %f\nmu_bsBEff <- %f\nsigma_bsBEff <- %f\nmu_oxEff <- %f\nsigma_oxEff <- %f\nmu_seqErr <- %f\nsigma_seqErr <- %f\n" % (mu_bsEff,sigma_bsEff,mu_bsBEff,sigma_bsBEff,mu_oxEff,sigma_oxEff,mu_seqErr,sigma_seqErr))
+    f.write("raw_bsEff <- c(%s)\nraw_bsBEff <- c(%s)\nraw_oxEff <- c(%s)\nraw_seqErr <- c(%s)\n" % (','.join(map(str,raw_bsEff)),','.join(map(str,raw_bsBEff)),','.join(map(str,raw_oxEff)),','.join(map(str,raw_seqErr))))
+    f.write("g <- %s\nf <- %s\n" % (str(g),str(f_v)))
+    f.write("theta <- structure(c(%s), .Dim=c(%d,%d,3))\n" % (theta,N,R))
+    f.write("theta_control <- structure(c(%s), .Dim=c(%d,%d,3))\n" % (theta_control,N_control,R))
+    f.write("nu <- structure(c(%s), .Dim=c(%d,3))\n" % (nu,N))
+    f.write("mu <- c(%s)\n" % (mu))
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Generates data and init files in the dump format for Lux')
+  parser.add_argument('-d','--data',action='store',dest='data',type=str,required=True,help='noncontrol cytosine data')
+  parser.add_argument('-p','--prior',action='store',dest='prior',type=str,required=True,help='prior of the noncontrol cytosines')
+  parser.add_argument('-cd','--control-data',action='store',dest='control_data',type=str,required=True,help='control cytosine data')
+  parser.add_argument('-cp','--control-prior',action='store',dest='control_prior',type=str,required=True,help='priors of the control cytosines')
+  parser.add_argument('-pr','--prefix',action='store',dest='prefix',type=str,required=True,help='prefix of the output files')
+  parser.add_argument('-v','--version',action='version',version='%(prog)s 0.666')
+
+  options = parser.parse_args()
+  if not os.path.isfile(options.data):
+    sys.exit('error: %s is not a file'%(options.data))
+  if not os.path.isfile(options.prior):
+    sys.exit('error: %s is not a file'%(options.prior))
+  if not os.path.isfile(options.control_data):
+    sys.exit('error: %s is not a file'%(options.control_data))
+  if not os.path.isfile(options.control_prior):
+    sys.exit('error: %s is not a file'%(options.control_prior))
+
+  generate_output_files(options.data,options.prior,options.control_data,options.control_prior,options.prefix)
